507ccf1a643dadfc7c2b6a04895daa052a29b8d3
Sebastian Plamauer <oeplse@gmail.com>
2017-10-23 08:27:44 +0000

    Add Python3 support

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from distutils.core import setup
 
 setup(name='Adafruit_BME280',
-      version='1.0',
-      description='Adafruit Python BME280 Liibrary',
-      url='https://github.com/adafruit/Adafruit_Python_BME280',
+      version='1.1',
+      description='Adafruit Python3 BME280 Library',
+      url='https://github.com/turbinenreiter/Adafruit_Python_BME280',
       py_modules=['Adafruit_BME280'],
      )
